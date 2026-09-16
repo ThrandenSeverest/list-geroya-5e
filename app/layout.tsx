07f89eafe00b39c541de2376c53c1874c89e63d2
ThrandenSeverest <37243264+ThrandenSeverest@users.noreply.github.com>
2026-09-16 23:20:31 +0300

    run storage guard before app hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { storageGuardScript } from "./storageGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: storageGuardScript }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
